1cdcf9bfc26c13e4da835d8075af8547d507d0ff
Rafael Guzman <rega1237@gmail.com>
2022-04-05 16:42:19 -0400

    add event listerner for display block links and for change the hamburguer button to x and viceversa

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ let btn =document.querySelector(".icon");
 let icon=btn.querySelector(".fa-bars")
 let x = document.getElementById("myLinks");
 let menu= false;
+
+btn.onclick=function (){
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      icon.classList.replace("fa-times","fa-bars");
+    } else {
+      x.style.display = "block";
+      icon.classList.replace("fa-bars","fa-times")
+    }
+   };
+     
+btn.addEventListener('click', () => {
+    if(!menuOpen) {
+      icon.classList.replace("fa-times","fa-bars");
+      menu = true;
+    } else {
+      icon.classList.replace("fa-bars","fa-times")
+      menu = false;
+     }
+   });
