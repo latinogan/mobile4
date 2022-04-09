2ed65b0b544ef3ce84a7940208a742c7b842b76c
latinogan <roger.castillo.arce@gmail.com>
2022-04-08 20:12:46 -0500

    add form.js

--- form.js
+++ form.js
@@ -1,12 +1,14 @@
-const emailAddress = document.getElementById('mail');
-const validatorMessage = document.getElementById('validator-message');
-const btn1 = document.querySelector('.get2');
+
+
+
 btn1.addEventListener('click', (e) => {
-  e.preventDefault();
   if (emailAddress.value !== emailAddress.value.toLowerCase()) {
     validatorMessage.textContent = 'pleas the input have to be in lowercase';
+    e.preventDefault();
     console.log('helloooo');
   } else {
     validatorMessage.textContent = '';
+    form.submit();
   }
+  
 });
